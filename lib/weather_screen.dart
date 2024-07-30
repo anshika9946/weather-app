a4cde87c69861d8432bc4753fbe6766171dac6d2
Anshika <anshika2011087@akgec.ac.in>
2024-07-30 23:50:47 +0530

    Created Current weather card

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -1,4 +1,7 @@
+import "dart:ui";
+
 import "package:flutter/material.dart";
+import "package:flutter/widgets.dart";
 
 class WeatherScreen extends StatelessWidget {
   const WeatherScreen({super.key});
@@ -20,6 +23,72 @@ class WeatherScreen extends StatelessWidget {
             icon: const Icon(Icons.refresh),
           )
         ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16.0),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16.0),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                    child: const Padding(
+                      padding: EdgeInsets.all(16),
+                      child: Column(
+                        children: [
+                          Text(
+                            '300F',
+                            style: TextStyle(
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 16),
+                          Icon(
+                            Icons.cloud,
+                            size: 55,
+                          ),
+                          SizedBox(height: 16),
+                          Text(
+                            'Rain',
+                            style: TextStyle(
+                              fontSize: 20,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Weather Forcast',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const Placeholder(
+              fallbackHeight: 120,
+            ),
+            const SizedBox(height: 20),
+            const Placeholder(
+              fallbackHeight: 120,
+            ),
+          ],
+        ),
       ),
     );
   }
