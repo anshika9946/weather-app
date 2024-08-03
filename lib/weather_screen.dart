01b756b1aa36853b17f17e573eac5974a25a5802
Anshika <anshika2011087@akgec.ac.in>
2024-08-03 18:27:26 +0530

    updated icons

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -1,12 +1,246 @@
-import "dart:convert";
-import "dart:ui";
+// import "dart:convert";
+// import "dart:ui";
 
-import "package:flutter/material.dart";
-import "package:intl/intl.dart";
-import "package:weatherapp/additional_info_item.dart";
-import "package:weatherapp/hourly_forecast_item.dart";
+// import "package:flutter/material.dart";
+// import "package:intl/intl.dart";
+// import "package:weatherapp/additional_info_item.dart";
+// import "package:weatherapp/hourly_forecast_item.dart";
+// import 'package:http/http.dart' as http;
+// import "package:weatherapp/secret.dart";
+
+// class WeatherScreen extends StatefulWidget {
+//   const WeatherScreen({super.key});
+
+//   @override
+//   State<WeatherScreen> createState() => _WeatherScreenState();
+// }
+
+// class _WeatherScreenState extends State<WeatherScreen> {
+//   late Future<Map<String, dynamic>> weather;
+
+//   Future<Map<String, dynamic>> getCurrentWeather() async {
+//     try {
+//       String cityName = 'Ghaziabad';
+
+//       final res = await http.get(
+//         Uri.parse(
+//             'https://api.openweathermap.org/data/2.5/forecast?q=$cityName&appid=$openWeatherAPIKey'),
+//       );
+//       final data = jsonDecode(res.body);
+
+//       if (data['cod'] != '200') {
+//         throw 'An unexpected error occurred';
+//       }
+//       return data;
+//       //data['list'][0]['main']['temp'];
+//     } catch (e) {
+//       throw e.toString();
+//     }
+//   }
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     weather = getCurrentWeather();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text(
+//           'Weather App',
+//           style: TextStyle(
+//             fontWeight: FontWeight.bold,
+//           ),
+//         ),
+//         centerTitle: true,
+//         actions: [
+//           IconButton(
+//             onPressed: () {
+//               setState(() {
+//                 weather = getCurrentWeather();
+//               });
+//             },
+//             icon: const Icon(Icons.refresh),
+//           )
+//         ],
+//       ),
+//       body: FutureBuilder(
+//         future: weather,
+//         builder: (context, snapshot) {
+//           if (snapshot.connectionState == ConnectionState.waiting) {
+//             return const Center(child: CircularProgressIndicator.adaptive());
+//           }
+//           if (snapshot.hasError) {
+//             return Center(child: Text(snapshot.error.toString()));
+//           }
+
+//           final data = snapshot.data!;
+//           final currentWeatherData = data['list'][0];
+
+//           final currentTemp = currentWeatherData['main']['temp'] - 273.15;
+//           final currentSky = currentWeatherData['weather'][0]['main'];
+//           final currentPressure = currentWeatherData['main']['pressure'];
+//           final currentHumidity = currentWeatherData['main']['humidity'];
+//           final currentWindSpeed = currentWeatherData['wind']['speed'];
+
+//           return Padding(
+//             padding: const EdgeInsets.all(16),
+//             child: Column(
+//               children: [
+//                 SizedBox(
+//                   width: double.infinity,
+//                   child: Card(
+//                     elevation: 10,
+//                     shape: RoundedRectangleBorder(
+//                       borderRadius: BorderRadius.circular(16.0),
+//                     ),
+//                     child: ClipRRect(
+//                       borderRadius: BorderRadius.circular(16.0),
+//                       child: BackdropFilter(
+//                         filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+//                         child: Padding(
+//                           padding: const EdgeInsets.all(16),
+//                           child: Column(
+//                             children: [
+//                               Text(
+//                                 '${currentTemp.toStringAsFixed(2)} °C',
+//                                 style: const TextStyle(
+//                                   fontSize: 32,
+//                                   fontWeight: FontWeight.bold,
+//                                 ),
+//                               ),
+//                               const SizedBox(height: 16),
+//                               Icon(
+//                                 currentSky == 'Clouds' || currentSky == 'Rain'
+//                                     ? Icons.cloud
+//                                     : Icons.sunny,
+//                                 size: 55,
+//                               ),
+//                               const SizedBox(height: 16),
+//                               Text(
+//                                 currentSky,
+//                                 style: const TextStyle(
+//                                   fontSize: 20,
+//                                 ),
+//                               )
+//                             ],
+//                           ),
+//                         ),
+//                       ),
+//                     ),
+//                   ),
+//                 ),
+//                 const SizedBox(height: 20),
+//                 const Align(
+//                   alignment: Alignment.centerLeft,
+//                   child: Text(
+//                     'Hourly Forcast',
+//                     style: TextStyle(
+//                       fontSize: 20,
+//                       fontWeight: FontWeight.bold,
+//                     ),
+//                   ),
+//                 ),
+//                 const SizedBox(height: 12),
+
+//                 //Weather forecast cards
+
+//                 // SingleChildScrollView(
+//                 //   scrollDirection: Axis.horizontal,
+//                 //   child: Row(
+//                 //     children: [
+//                 //       for (int i = 0; i < 6; i++)
+//                 //         HourlyForecastItem(
+//                 //           time: data['list'][i + 1]['dt'].toString(),
+//                 //           icon: data['list'][i + 1]['weather'][0]['main'] ==
+//                 //                       'Clouds' ||
+//                 //                   data['list'][i + 1]['weather'][0]['main'] ==
+//                 //                       'Rain'
+//                 //               ? Icons.cloud
+//                 //               : Icons.sunny,
+//                 //           degree:
+//                 //               '${(data['list'][i + 1]['main']['temp'] - 273.15).toStringAsFixed(2)}  °C',
+//                 //         ),
+//                 //     ],
+//                 //   ),
+//                 // ), but all card and data accessed at the single time not an efficient way so need lazy loading
+
+//                 SizedBox(
+//                   height: 120,
+//                   child: ListView.builder(
+//                     scrollDirection: Axis.horizontal,
+//                     itemCount: 5,
+//                     itemBuilder: (context, index) {
+//                       final hourlyForecast = data['list'][index + 1];
+//                       final hourlySky =
+//                           data['list'][index + 1]['weather'][0]['main'];
+//                       final hourlyTemp =
+//                           (hourlyForecast['main']['temp'] - 273.15)
+//                               .toStringAsFixed(2);
+//                       final time = DateTime.parse(hourlyForecast['dt_txt']);
+//                       return HourlyForecastItem(
+//                         icon: hourlySky == 'Clouds' || hourlySky == 'Rain'
+//                             ? Icons.cloud
+//                             : Icons.sunny,
+//                         degree: '$hourlyTemp °C',
+//                         time: DateFormat.j().format(time),
+//                       );
+//                     },
+//                   ),
+//                 ),
+
+//                 const SizedBox(height: 20),
+//                 const Align(
+//                   alignment: Alignment.centerLeft,
+//                   child: Text(
+//                     'Additional Information',
+//                     style: TextStyle(
+//                       fontSize: 20,
+//                       fontWeight: FontWeight.bold,
+//                     ),
+//                   ),
+//                 ),
+//                 const SizedBox(height: 12),
+//                 Row(
+//                   mainAxisAlignment: MainAxisAlignment.spaceAround,
+//                   children: [
+//                     AdditionalInfoItem(
+//                       icon: Icons.water_drop,
+//                       label: 'Humidity',
+//                       value: currentHumidity.toString(),
+//                     ),
+//                     AdditionalInfoItem(
+//                       icon: Icons.air,
+//                       label: 'Wind Speed',
+//                       value: currentWindSpeed.toString(),
+//                     ),
+//                     AdditionalInfoItem(
+//                       icon: Icons.beach_access,
+//                       label: 'Pressure',
+//                       value: currentPressure.toString(),
+//                     ),
+//                   ],
+//                 ),
+//               ],
+//             ),
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
+
+import 'dart:convert';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:weatherapp/additional_info_item.dart';
+import 'package:weatherapp/hourly_forecast_item.dart';
 import 'package:http/http.dart' as http;
-import "package:weatherapp/secret.dart";
+import 'package:weatherapp/secret.dart';
 
 class WeatherScreen extends StatefulWidget {
   const WeatherScreen({super.key});
@@ -26,15 +260,20 @@ class _WeatherScreenState extends State<WeatherScreen> {
         Uri.parse(
             'https://api.openweathermap.org/data/2.5/forecast?q=$cityName&appid=$openWeatherAPIKey'),
       );
-      final data = jsonDecode(res.body);
 
-      if (data['cod'] != '200') {
-        throw 'An unexpected error occurred';
+      if (res.statusCode != 200) {
+        throw Exception('Failed to load weather data: ${res.statusCode}');
       }
+
+      final data = jsonDecode(res.body);
+      if (data['cod'] != '200') {
+        throw Exception('Error: ${data['message']}');
+      }
+
       return data;
-      //data['list'][0]['main']['temp'];
     } catch (e) {
-      throw e.toString();
+      print('Error fetching weather data: $e');
+      rethrow;
     }
   }
 
@@ -66,14 +305,14 @@ class _WeatherScreenState extends State<WeatherScreen> {
           )
         ],
       ),
-      body: FutureBuilder(
+      body: FutureBuilder<Map<String, dynamic>>(
         future: weather,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator.adaptive());
           }
           if (snapshot.hasError) {
-            return Center(child: Text(snapshot.error.toString()));
+            return Center(child: Text('Error: ${snapshot.error}'));
           }
 
           final data = snapshot.data!;
@@ -113,9 +352,11 @@ class _WeatherScreenState extends State<WeatherScreen> {
                               ),
                               const SizedBox(height: 16),
                               Icon(
-                                currentSky == 'Clouds' || currentSky == 'Rain'
+                                currentSky == 'Clouds'
                                     ? Icons.cloud
-                                    : Icons.sunny,
+                                    : currentSky == 'Rain'
+                                        ? Icons.thunderstorm_rounded
+                                        : Icons.sunny,
                                 size: 55,
                               ),
                               const SizedBox(height: 16),
@@ -136,7 +377,7 @@ class _WeatherScreenState extends State<WeatherScreen> {
                 const Align(
                   alignment: Alignment.centerLeft,
                   child: Text(
-                    'Hourly Forcast',
+                    'Hourly Forecast',
                     style: TextStyle(
                       fontSize: 20,
                       fontWeight: FontWeight.bold,
@@ -144,29 +385,6 @@ class _WeatherScreenState extends State<WeatherScreen> {
                   ),
                 ),
                 const SizedBox(height: 12),
-
-                //Weather forecast cards
-
-                // SingleChildScrollView(
-                //   scrollDirection: Axis.horizontal,
-                //   child: Row(
-                //     children: [
-                //       for (int i = 0; i < 6; i++)
-                //         HourlyForecastItem(
-                //           time: data['list'][i + 1]['dt'].toString(),
-                //           icon: data['list'][i + 1]['weather'][0]['main'] ==
-                //                       'Clouds' ||
-                //                   data['list'][i + 1]['weather'][0]['main'] ==
-                //                       'Rain'
-                //               ? Icons.cloud
-                //               : Icons.sunny,
-                //           degree:
-                //               '${(data['list'][i + 1]['main']['temp'] - 273.15).toStringAsFixed(2)}  °C',
-                //         ),
-                //     ],
-                //   ),
-                // ), but all card and data accessed at the single time not an efficient way so need lazy loading
-
                 SizedBox(
                   height: 120,
                   child: ListView.builder(
@@ -190,7 +408,6 @@ class _WeatherScreenState extends State<WeatherScreen> {
                     },
                   ),
                 ),
-
                 const SizedBox(height: 20),
                 const Align(
                   alignment: Alignment.centerLeft,
